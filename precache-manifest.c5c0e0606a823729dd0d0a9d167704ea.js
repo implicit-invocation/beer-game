885b96e2b4e1fb62aa0e6f4f219cbbb48e42af1e
Duong Dat <tao@cu8-3.local>
2020-02-21 14:10:00 +0700

    first commit

---
+++ precache-manifest.c5c0e0606a823729dd0d0a9d167704ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ab799905e5fd876ddd4b6cbda6923f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a7ef1a078d061ee28ad",
+    "url": "./static/css/main.ed04e37a.chunk.css"
+  },
+  {
+    "revision": "5dc8bc56b290c6c77870",
+    "url": "./static/js/2.3ed2429e.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "./static/js/2.3ed2429e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7ef1a078d061ee28ad",
+    "url": "./static/js/main.e2182c08.chunk.js"
+  },
+  {
+    "revision": "7dd5fb9ce3fc626273e3",
+    "url": "./static/js/runtime-main.a8fddc83.js"
+  }
+]);
